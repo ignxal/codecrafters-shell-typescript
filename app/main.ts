6c04b63de83747541785fb3e1d77cf84490a15a1
ignxal <ignacio.logiudice@gmail.com>
2026-01-03 17:01:15 -0300

    feat: add type cmd

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,7 @@
 import { createInterface } from "readline";
 
+const BUILTIN_COMMANDS = ["exit", "echo", "type"];
+
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -17,8 +19,9 @@ function handleAnswer(answer: string): boolean {
     exit();
     return false;
   } else if (answer.startsWith("echo ")) {
-    const echoContent = answer.slice(5);
-    echo(echoContent);
+    echo(answer);
+  } else if (answer.startsWith("type ")) {
+    type(answer);
   } else {
     console.log(`${answer}: command not found`);
   }
@@ -31,7 +34,21 @@ function exit(): void {
 }
 
 function echo(input: string): void {
-  console.log(input);
+  const content = extractContent(input, "echo ");
+  console.log(content);
+}
+
+function type(input: string): void {
+  const content = extractContent(input, "type ");
+  if (BUILTIN_COMMANDS.includes(content)) {
+    return console.log(`${content} is a shell builtin`);
+  }
+
+  console.log(`${content}: not found`);
+}
+
+function extractContent(command: string, prefix: string): string {
+  return command.split(prefix)[1];
 }
 
 function main() {
