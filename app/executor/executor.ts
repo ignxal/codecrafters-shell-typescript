01df02d4c1096462e19c25df628ae22df2f5cbc1
ignxal <ignacio.logiudice@gmail.com>
2026-01-04 00:14:33 -0300

    fix: run ussing command name instead of path

--- app/executor/executor.ts
+++ app/executor/executor.ts
@@ -15,21 +15,10 @@ export async function executeExternal(
     return 1;
   }
 
-  //const programArgCount = args.length + 1;
-  //const lines: string[] = [];
-  //lines.push(
-  //  `Program was passed ${programArgCount} args (including program name).`
-  //);
-  //lines.push(`Arg #0 (program name): ${command}`);
-  //args.forEach((x, i) => {
-  //  lines.push(`Arg #${i + 1}: ${x}`);
-  //});
-  //
-  //console.log(lines.join("\n"));
-
   const result = spawnSync(path, args, {
     stdio: "inherit",
     shell: false,
+    argv0: command,
   });
 
   return result.status ?? 1;
