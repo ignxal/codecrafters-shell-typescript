5432ba7f75d33c75a7b8c2781a299e7dff1f5de3
ignxal <ignacio.logiudice@gmail.com>
2026-01-03 22:01:29 -0300

    refact: added Shell class

---
+++ app/shell.ts
@@ -0,0 +1,50 @@
+import { builtins } from "./builtins";
+import { extractContent } from "./utils/parser";
+import * as readline from "readline";
+
+export class Shell {
+  private rl: readline.Interface;
+
+  constructor() {
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+  }
+
+  async run(): Promise<void> {
+    while (true) {
+      const input = await this.askQuestion("$ ");
+      const shouldExit = await this.handleAnswer(input);
+
+      if (shouldExit) {
+        this.rl.close();
+        break;
+      }
+    }
+  }
+
+  private askQuestion(prompt: string): Promise<string> {
+    return new Promise((resolve) => {
+      this.rl.question(prompt, resolve);
+    });
+  }
+
+  private async handleAnswer(input: string): Promise<boolean> {
+    const { command, args } = extractContent(input);
+
+    if (!command) return false;
+
+    if (builtins[command]) {
+      const result = await builtins[command](args);
+      if (result.shouldExit) {
+        process.exitCode = result.exitCode;
+        return true;
+      }
+    } else {
+      console.log(`${input}: command not found`);
+    }
+
+    return false;
+  }
+}
