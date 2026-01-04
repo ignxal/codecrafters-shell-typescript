6bcf3d898e6c4a6c1f73121e5b1f499f1242311e
ignxal <ignacio.logiudice@gmail.com>
2026-01-03 23:50:49 -0300

    fix: missing log on executeExternal

--- app/executor/executor.ts
+++ app/executor/executor.ts
@@ -11,6 +11,27 @@ export async function executeExternal(
     return 127;
   }
 
+  const programArgCount = args.length + 1;
+  const lines: string[] = [];
+  lines.push(
+    `Program was passed ${programArgCount} args (including program name).`
+  );
+  lines.push(`Arg #0 (program name): ${command}`);
+  args.forEach((x, i) => {
+    lines.push(`Arg #${i + 1}: ${x}`);
+  });
+
+  const programSignature = args.length
+    ? `${command} ${args.join(" ")}`
+    : command;
+
+  console.log(
+    lines.join("\n") +
+      `
+
+Program Signature: ${programSignature}`
+  );
+
   const result = spawnSync(path, args, {
     stdio: "inherit",
     shell: false,
